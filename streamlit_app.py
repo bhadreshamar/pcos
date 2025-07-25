b63d3a07a0304c502181b58af89232babec52e91
bhadreshamar <baresearchacademy@gmail.com>
2025-07-25 00:54:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,32 @@ def plot_data(df):
     plot.set_ylabel("Frequency")
     plt.show()
         
+def get_model():
+    from sklearn.model_selection import train_test_split
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.metrics import accuracy_score, classification_report
+
+    df = get_clean_data()
+
+    # scale predictors and split data
+    X = df.drop(['diagnosis'], axis=1)
+    y = df['diagnosis']
+    from sklearn.preprocessing import StandardScaler
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42)
+
+    # train the model
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+
+    # test the model
+    y_pred = model.predict(X_test)
+    print("Accuracy: ", accuracy_score(y_test, y_pred))
+    print("Classification report: \n", classification_report(y_test, y_pred))
+    return model, scaler
 
 def add_sidebar():
   st.sidebar.header("Patient PCOS Dataset Details")
