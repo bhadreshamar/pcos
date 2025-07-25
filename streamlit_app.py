803e2ab11149b8fec274542374ca6add183ae195
amuddula <amuddula@gmail.com>
2025-07-24 23:45:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,10 +24,10 @@ with st.container():
     st.title("Breast Cancer Diagnosis")
     st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar. ")
     col1, col2 = st.columns([4,1])
-        with col1:
-            st.write("Column 1")
-        with col2:
-            st.write("Column 2")
+    with col1:
+        st.write("Column 1")
+    with col2:
+        st.write("Column 2")
         
 
 
