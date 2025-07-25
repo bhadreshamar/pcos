6ccbd4d69dc5f7d6db4c338e7d61266867d5ffdd
bhadreshamar <baresearchacademy@gmail.com>
2025-07-24 23:39:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ def get_clean_data():
 
 
 # Add a title
-st.set_page_config(page_title="Breast Cancer Diagnosis",
+st.set_page_config(page_title="Detect PCOS",
                     page_icon="👩‍⚕️", 
                     layout="wide", 
                     initial_sidebar_state="expanded")
