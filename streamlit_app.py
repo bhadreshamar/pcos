a77a84de519440361f266569968ccb2e70f9b6fa
amuddula <amuddula@gmail.com>
2025-07-24 23:22:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import plotly.graph_objects as go
+import numpy as np
+
+
+def get_clean_data():
+  data = pd.read_csv("data/data.csv")
+  
+  data = data.drop(['Unnamed: 32', 'id'], axis=1)
+  
+  data['diagnosis'] = data['diagnosis'].map({ 'M': 1, 'B': 0 })
+  
+  return data
 
 st.write ('PCOS')
 
