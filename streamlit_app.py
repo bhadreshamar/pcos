7e1c9e61eb4f11929205fc1313834c325f36685b
amuddula <amuddula@gmail.com>
2025-07-24 23:41:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,12 @@ st.set_page_config(page_title="Detect PCOS",
                     layout="wide", 
                     initial_sidebar_state="expanded")
 
+# Set up the structure
+    with st.container():
+        st.title("Breast Cancer Diagnosis")
+        st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar. ")
+
+
 st.write ('PCOS')
 
 st.info ('This app builds a machine learning model to detect PCOS')
