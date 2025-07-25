ba86ed7e15b8a0e9eea32e72e638c3a4c1d8d7d8
bhadreshamar <baresearchacademy@gmail.com>
2025-07-24 23:35:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import pandas as pd
 def get_clean_data():
   data = pd.read_csv("Data/PCOS_data_infertility.csv")
   
-  data = data.drop(['Unnamed: 32', 'id'], axis=1)
+  data = data.drop(['Unnamed: 44', 'Si. No'], axis=1)
   
   data['diagnosis'] = data['diagnosis'].map({ 'M': 1, 'B': 0 })
   
